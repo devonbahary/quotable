56c6f81656e3d1af69e0afa81ae99b911540e31f
Devon Bahary <devon.bahary@robinhealthcare.com>
2020-02-14 19:18:10 -0800

    styles

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
 import View from "./View";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+
+import styles from "./styles/quotes.scss";
 
 const Quotes = observer(({ store }) => {
     const { quotes } = store;
 
     return (
         <View>
-            <h2>Quotes</h2>
             <ul>
                 {quotes.map(q => (
-                    <li key={q.id}>
+                    <li key={q.id} className={styles.quote}>
+                        <span className={styles.quoteLeft}>
+                            <FontAwesomeIcon icon={faQuoteLeft} size='xs' />
+                        </span>
                         {q.text}
+                        <span className={styles.quoteRight}>
+                            <FontAwesomeIcon icon={faQuoteRight} size='xs' />
+                        </span>
                     </li>
                 ))}
             </ul>
