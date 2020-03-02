8b807bb7eaaa254ca7eb02cb1dfdcb6e2157d8e8
Devon Bahary <devon.bahary@robinhealthcare.com>
2020-03-01 18:03:50 -0800

    adds missing return statement in api.js

--- src/api.js
+++ src/api.js
@@ -44,14 +44,14 @@ export const updateQuoteById = async (id, text) => {
 };
 
 export const saveNewQuote = async quote => {
-    errorHandler(async () => {
+    return errorHandler(async () => {
         const { data } = await axios.post(QUOTES_PATH, quote);
         return data;
     });
 };
 
 export const deleteQuote = async quote => {
-    errorHandler(async () => {
+    return errorHandler(async () => {
         await axios.delete(`${QUOTES_PATH}/${quote.id}`);
     });
 };
