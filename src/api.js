09a7961c2f066adab35eece6336ffa1193940d02
Devon Bahary <devon.bahary@robinhealthcare.com>
2020-03-01 17:07:24 -0800

    wraps all calls to back-end in errorHandler()

--- src/api.js
+++ src/api.js
@@ -3,54 +3,55 @@ import axios from "axios";
 const COLLECTIONS_PATH = `/api/collections`;
 const QUOTES_PATH = `/api/quotes`;
 
+
+const errorHandler = cb => {
+    try {
+        return cb();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
 export const authenticateUser = async token => {
     const { data: jwt } = await axios.post('/api/authentication/token', { token });
     axios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
 };
 
 export const getUserCollections = async () => {
-    const { data: collections } = await axios.get(COLLECTIONS_PATH);
-    return collections;
+    return errorHandler(async () => {
+        const { data: collections } = await axios.get(COLLECTIONS_PATH);
+        return collections;
+    });
 };
 
 export const updateCollectionById = async (id, title) => {
-    try {
+    return errorHandler(async () => {
         await axios.put(`${COLLECTIONS_PATH}/${id}`, { title });
-    } catch (err) {
-        console.error(err);
-    }
+    });
 };
 
 export const getUserQuotes = async () => {
-    try {
+    return errorHandler(async () => {
         const { data: quotes } = await axios.get(QUOTES_PATH);
         return quotes;
-    } catch (err) {
-        console.error(err);
-    }
+    });
 };
 
 export const updateQuoteById = async (id, text) => {
-    try {
+    return errorHandler(async () => {
         await axios.put(`${QUOTES_PATH}/${id}`, { text });
-    } catch (err) {
-        console.error(err);
-    }
+    });
 };
 
 export const saveNewQuote = async quote => {
-    try {
+    errorHandler(async () => {
         const { data } = await axios.post(QUOTES_PATH, quote);
         return data;
-    } catch (err) {
-        console.error(err);
-    }
+    });
 };
 
 export const deleteQuote = async quote => {
-    try {
+    errorHandler(async () => {
         await axios.delete(`${QUOTES_PATH}/${quote.id}`);
-    } catch (err) {
-        console.error(err);
-    }
+    });
 };
