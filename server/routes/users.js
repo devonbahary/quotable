738b3f2201ef756b377b22b407e2cbfd81529674
Devon Bahary <devon.bahary@robinhealthcare.com>
2020-03-10 00:26:05 -0700

    removes test web push notification from /api/users/subscribe

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,10 @@
 import express from "express";
-import webpush from "web-push";
 import UsersRepository from "../repositories/UsersRepository";
 import { validateUser } from "../auth";
 import { errorHandler } from "./util";
-import config from "../config";
 
 const router = express.Router();
 const usersRepository = new UsersRepository();
-
-
-webpush.setVapidDetails(`mailto:${config.vapid.emailAddress}`, config.vapid.publicKey, config.vapid.privateKey);
 
 
 router.get('/me', validateUser, (req, res) => {
@@ -29,16 +24,8 @@ router.post('/subscribe', validateUser, async (req, res) => {
 
     errorHandler(res, async () => {
         await usersRepository.updatePushNotificationSubscription(req.user.id, JSON.stringify(subscription));
-
-        res.status(201).send({});
+        res.sendStatus(201);
     });
-
-    // TODO: temporary testing
-    const payload = JSON.stringify({ title: 'Test Notification Title' });
-
-    setTimeout(async () => {
-        await webpush.sendNotification(subscription, payload).catch(err => console.error(err));
-    }, 10000);
 });
 
 export default router;
