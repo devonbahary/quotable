960b3b8af981de54fa0d58814ec3d3620343413e
Devon Bahary <devon.bahary@robinhealthcare.com>
2020-03-10 00:45:43 -0700

    adds try / catch around assignDailyQuotes.js

--- server/tasks/assignDailyQuotes.js
+++ server/tasks/assignDailyQuotes.js
@@ -4,6 +4,10 @@ const UsersService = require('../services/UsersService').default;
 const usersService = new UsersService();
 
 (async () => {
-    await usersService.assignRandomDailyQuotes();
+    try {
+        await usersService.assignRandomDailyQuotes();
+    } catch (err) {
+        console.err(`assignDailyQuotes.js error: ${JSON.stringify(err)}`);
+    }
     process.exit(1);
 })();
