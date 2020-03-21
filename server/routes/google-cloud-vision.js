c411d0a6a4ccdbad4b4f0c517138e27bafee61d9
Devon Bahary <devon.bahary@robinhealthcare.com>
2020-03-21 02:21:16 -0700

    removes error logging

--- server/routes/google-cloud-vision.js
+++ server/routes/google-cloud-vision.js
@@ -11,7 +11,6 @@ const router = express.Router();
 router.post('/', (req, res) => {
     errorHandler(res, async () => {
         const { image } = req.body;
-        console.log(JSON.stringify(image));
         const imageBase64 = image.split(',')[1];
 
         const request = {
